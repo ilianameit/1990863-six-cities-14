aebc515ea6085097aa3c0260e0c71b1af2c9a515
iliana <ilianameit@gmail.com>
2023-11-06 17:04:03 +0300

    хук use map

---
+++ src/hooks/use-map/use-map.tsx
@@ -0,0 +1,35 @@
+import { MutableRefObject, useEffect, useRef, useState } from 'react';
+import { City } from '../../types/city';
+import { Map, TileLayer } from 'leaflet';
+import { TileLayerSettings } from './use-map.const';
+
+export function useMap(
+  mapRef: MutableRefObject<HTMLElement | null>,
+  city: City
+): Map | null {
+  const [map, setMap] = useState<Map | null>(null);
+  const isRenderedRef = useRef<boolean>(false);
+
+  useEffect(() => {
+    if (mapRef.current !== null && !isRenderedRef.current) {
+      const instance = new Map(mapRef.current, {
+        center: {
+          lat: city.location.latitude,
+          lng: city.location.longitude,
+        },
+        zoom: city.location.zoom,
+      });
+
+      const layer = new TileLayer(TileLayerSettings.url, {
+        attribution: TileLayerSettings.attribution
+      });
+
+      instance.addLayer(layer);
+
+      setMap(instance);
+      isRenderedRef.current = true;
+    }
+  }, [mapRef, city]);
+
+  return map;
+}
