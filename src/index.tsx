15ebea27862ef11221c07278c2041cd119ad3611
iliana <ilianameit@gmail.com>
2023-11-12 20:35:48 +0300

    создание хука и подключение глобального состояния

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import { offers } from './mocks/offers';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <App offers={offers}/>
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
+  </Provider>
 );
