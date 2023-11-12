8dac8b5a92a4d42889e790727368cd8a5725cd3c
iliana <ilianameit@gmail.com>
2023-11-12 15:48:01 +0300

    доработка location

--- src/components/locations/locations.tsx
+++ src/components/locations/locations.tsx
@@ -4,13 +4,11 @@ import { City } from '../../types/city';
 import classNames from 'classnames';
 
 //getParametr исправить маршрутизацию
-
 type LocationsProp = {
   activeCity: City['name'];
-  onCityClick: (city: City['name']) => void;
 }
 //остаются странные полосы при переключении города
-function Locations({activeCity, onCityClick}: LocationsProp): JSX.Element {
+function Locations({activeCity, }: LocationsProp): JSX.Element {
   return(
     <section className="locations container">
       <ul className="locations__list tabs__list">
@@ -26,7 +24,6 @@ function Locations({activeCity, onCityClick}: LocationsProp): JSX.Element {
                 'tabs__item',
                 {'tabs__item--active' : activeCity === city}
               )}
-              onClick={() =>onCityClick(city)}
             >
               <span>{city}</span>
             </Link>
